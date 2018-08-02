e6b8c2ac9a322260f120df0186903da6ce9e5364
CHIp Wright <frisbeeprod@msn.com>
2018-08-02 00:54:30 -0700

    slogan css and styling

--- src/components/Slogan.js
+++ src/components/Slogan.js
@@ -7,13 +7,13 @@ class Slogan extends Component {
     return (
       <div>
         <div className="Parallax1"></div>
-        <div className="Center">
+        <Jumbotron className="Center">
           <h3>Hello BreAnne!</h3>
           <div>UNIQUE EXPERIENCE</div>
           <div>INNOVATIVE TECHNOLOGIES</div>
           <h4>NO SERVER ISSUES CAUSING EXTRA WORK!</h4>
           <h4>BUILDING A BETTER, SAFER, MORE RESILIENT FUTURE</h4>
-        </div>
+        </Jumbotron>
         <div className="Parallax2"></div>
         <video src="https://player.vimeo.com/video/279141793?title=0&portrait=0&muted=1&autopause=0&loop=1&background=1&app_id=122963" autoplay loop type="video/mp4" className="Parallax3">
         </video>
