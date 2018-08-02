9c4b15ff9a902d547c8ffda549af84905c702da9
CHIp Wright <frisbeeprod@msn.com>
2018-08-02 02:23:26 -0700

    lorem ipsum in middle of 2nd frame

--- src/components/Slogan.js
+++ src/components/Slogan.js
@@ -6,19 +6,39 @@ class Slogan extends Component {
   render() {
     return (
       <div>
-        <div className="Parallax1"></div>
+        <div className="Parallax1" />
         <Jumbotron className="Parallax1">
-           <div className="Center">
-             <h3>Hello BreAnne!</h3>
-             <div>UNIQUE EXPERIENCE</div>
-             <div>INNOVATIVE TECHNOLOGIES</div>
-             <h4>BUILDING A BETTER, SAFER, MORE RESILIENT FUTURE</h4>
-             <h4>AlSO, NO SERVER ISSUES CAUSING EXTRA WORK!</h4>
+          <div className="Center">
+            <h3>Hello BreAnne!</h3>
+            <div>UNIQUE EXPERIENCE</div>
+            <div>INNOVATIVE TECHNOLOGIES</div>
+            <h4>BUILDING A BETTER, SAFER, MORE RESILIENT FUTURE</h4>
+            <h4>AlSO, NO SERVER ISSUES CAUSING EXTRA WORK!</h4>
           </div>
         </Jumbotron>
-        <div className="Parallax1"></div>
-        <div className="Parallax2"></div>
-        <iframe className="Parallax3" src="https://player.vimeo.com/video/279141793?title=0&amp;portrait=0&amp;muted=1&amp;autopause=0&amp;loop=1&amp;background=1&amp;app_id=122963" width="1300px" frameborder="0" title="Big X Case Study" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" data-ready="true"></iframe>
+        <div className="Parallax1" />
+        <div className="Parallax2" />
+        <Jumbotron className="Center2">
+          <p>
+            Latin rambling:
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </Jumbotron>
+        <div className="Parallax2" />
+        <iframe
+          className="Parallax3"
+          src="https://player.vimeo.com/video/279141793?title=0&amp;portrait=0&amp;muted=1&amp;autopause=0&amp;loop=1&amp;background=1&amp;app_id=122963"
+          width="1300px"
+          frameBorder="0"
+          title="Big X Case Study"
+          webkitallowfullscreen=""
+          mozallowfullscreen=""
+          allowFullScreen=""
+          data-ready="true"
+        />
       </div>
     );
   }
